7be5780b42cf2a306c6d4b6548da02af688e40f3
Luan-art <luanlf2003@gmail.com>
2024-04-25 11:34:09 -0300

    novas funcoes

--- somasMatriz/Program.cs
+++ somasMatriz/Program.cs
@@ -1,97 +1,195 @@
-﻿int qtdLinha = 2, opcao = 0, resultado = 0;
-int[,] matriz = new int[qtdLinha, qtdLinha];
+﻿int linhaColunas = EscolherTamanhoMatriz();
+int[,] matriz;
 
-do
+int EscolherTamanhoMatriz()
 {
-    Console.WriteLine("Digite se deja aleatorizar as matrizes [1] ou colocar os dados a mão [2]: ");
+    int tamanho;
+    do
+    {
+        Console.WriteLine("Digite o numero de linhas da Matriz(diferente de 0): ");
+        tamanho = int.Parse(Console.ReadLine());
+
+    } while (tamanho <= 0);
+
+    return tamanho;
+}
+
+int EscolherFormaInsercaoDados()
+{
+    int opcao = 0; 
+    do
+    {
+        Console.WriteLine("Digite se deja aleatorizar as matrizes [1] ou colocar os dados a mão [2]: ");
+        opcao = int.Parse(Console.ReadLine());
+
+    } while (opcao != 1 && opcao != 2);
+
+    return opcao;
+}
+
+int[,] insercaoDados(int tipoInsercao, int linhaColunas)
+{
+    int[,] matrizPreenchida = new int[linhaColunas, linhaColunas];
+
+    switch (tipoInsercao)
+    {
+        case 1:
+            for (int linha = 0; linha < linhaColunas; linha++)
+                for (int coluna = 0; coluna < linhaColunas; coluna++)
+                {
+                    matrizPreenchida[linha, coluna] = new Random().Next(0, 10);
+                }
+            break;
+        case 2:
+            for (int linha = 0; linha < linhaColunas; linha++)
+                for (int coluna = 0; coluna < linhaColunas; coluna++)
+                {
+                    Console.WriteLine("Digite os valores da primeira Matriz");
+                    matrizPreenchida[linha, coluna] = int.Parse(Console.ReadLine());
+
+                }
+            break;
+        default:
+            Console.WriteLine("Invalido");
+            break;
+    }
+    return matrizPreenchida;
+}
+int EscolherMenu(){
+
+    int opcao;
+    Console.WriteLine("\nDigite se Deseja Soma as Linhas [1]");
+    Console.WriteLine("Digite se Deseja Soma as Calunas [2]");
+    Console.WriteLine("Digite se Deseja Soma a Diagonaç=l [3]");
+    Console.WriteLine("Digite se Deseja Soma a Diagonal Inversa[4]");
+
     opcao = int.Parse(Console.ReadLine());
 
-} while (opcao != 1 && opcao != 2);
+    return opcao;
+}
 
-switch (opcao)
+void SomarLinhas()
+{
+    int resultado = 0;
+    for (int linha = 0; linha < linhaColunas; linha++)
+    {
+        resultado = 0;
+        for (int coluna = 0; coluna < linhaColunas; coluna++)
+            resultado += matriz[linha, coluna];
+
+        Console.WriteLine("Soma linhas: " + resultado);
+
+    }
+
+}
+
+void SomarColunas()
+{
+    int resultado = 0;
+    for (int coluna = 0; coluna < linhaColunas; coluna++)
+    {
+        resultado = 0;
+        for (int linha = 0; linha < linhaColunas; linha++)
+            resultado += matriz[linha, coluna];
+
+        Console.WriteLine("Soma Colunas: " + resultado);
+
+    }
+
+}
+
+void SomarDiagonal()
+{
+    int resultado = 0;
+    for (int coluna = 0, linha = 0; coluna < linhaColunas; coluna++, linha++)
+    {
+        resultado += matriz[linha, coluna];
+
+
+    }
+    Console.WriteLine("Soma diagonal: " + resultado);
+}
+
+void SomarDiagonalReversa()
+{
+    int resultado = 0;
+
+    for (int coluna = linhaColunas - 1, linha = 0; coluna >= 0 && linha < linhaColunas; coluna--, linha++)
+    {
+        resultado += matriz[linha, coluna];
+
+    }
+    Console.WriteLine("Soma diagonal Inversa: " + resultado);
+
+}
+
+void ImprimirMatriz(int[,] m1)
+{
+
+    Console.WriteLine("\nMatriz");
+    for (int linha = 0; linha < linhaColunas; linha++)
+    {
+        Console.WriteLine();
+        for (int coluna = 0; coluna < linhaColunas; coluna++)
+            Console.Write(m1[linha, coluna] + " ");
+
+    }
+
+}
+
+matriz = new int[linhaColunas, linhaColunas];
+
+switch (EscolherFormaInsercaoDados())
 {
     case 1:
-        for (int linha = 0; linha < qtdLinha; linha++)
-            for (int coluna = 0; coluna < qtdLinha; coluna++)
-            {
-                matriz[linha, coluna] = new Random().Next(1, 100);
-            }
+        matriz = insercaoDados( 1 , linhaColunas);
         break;
 
     case 2:
-        for (int linha = 0; linha < qtdLinha; linha++)
-            for (int coluna = 0; coluna < qtdLinha; coluna++)
-            {
-                Console.WriteLine("Digite os valores da primeira Matriz");
-                matriz[linha, coluna] = int.Parse(Console.ReadLine());
-
-            }
+        matriz = insercaoDados( 2 , linhaColunas);
         break;
 
     default:
+        Console.WriteLine("Invalido");
         break;
 }
 
-Console.WriteLine("\nMatriz");
-for (int linha = 0; linha < qtdLinha; linha++)
+ImprimirMatriz(matriz);
+do
 {
-    Console.WriteLine();
-    for (int coluna = 0; coluna < qtdLinha; coluna++)
-        Console.Write(matriz[linha, coluna] + " ");
+    switch (EscolherMenu())
+    {
+        case 1:
+            SomarLinhas();
+            break;
 
-}
+        case 2:
+            SomarColunas();
+            break;
 
-Console.WriteLine();
-Console.WriteLine();
+        case 3:
+            SomarDiagonal();
+            break;
 
-for (int linha = 0; linha < qtdLinha; linha++)
-{
-    resultado = 0;
-    for (int coluna = 0; coluna < qtdLinha; coluna++)
-        resultado += matriz[linha, coluna];
-
-    Console.WriteLine("Soma linhas: " + resultado);
-
-}
-
-Console.WriteLine();
-Console.WriteLine();
-
-for (int coluna = 0; coluna < qtdLinha; coluna++)
-{
-    resultado = 0;
-    for (int linha = 0; linha < qtdLinha; linha++)
-        resultado += matriz[linha, coluna];
-
-    Console.WriteLine("Soma Colunas: " + resultado);
-
-}
+        case 4:
+            SomarDiagonalReversa();
+            break;
 
 
-Console.WriteLine();
-Console.WriteLine();
-resultado = 0;
+        default:
+            Console.WriteLine("Invalido");
+            break;
+    }
 
-for (int coluna = 0, linha = 0; coluna < qtdLinha; coluna++, linha++)
-{
-        resultado += matriz[linha, coluna];
-
-
-}
-Console.WriteLine("Soma diagonal: " + resultado);
-
-
-Console.WriteLine();
-Console.WriteLine();
-resultado = 0;
-
-for (int coluna = qtdLinha - 1, linha = 0 ; coluna >= 0 && linha < qtdLinha ; coluna--, linha++)
-{
-    resultado += matriz[linha, coluna];
-
-}
-Console.WriteLine("Soma diagonal Inversa: " + resultado);
+}while(true);
 
 
 
-Console.WriteLine();
+
+
+
+
+
+
+
+
